11ca1c13569652801aafb7bc8dd34cc7dd77f28c
Cristiano Corrado <cristiano.corrado@gmail.com>
2021-04-09 13:01:01 +0100

    Added secrets to test regexes

---
+++ conf/db_conn.py
@@ -0,0 +1,2 @@
+mongo://useradmin:password123@mongo.host.example.com/db1
+git://UUUuser:GGGpass@github.com
--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import os
 password="asdf"
 secret = "verysecret"
 
+password: ${test}
 
+
+def auth(user: "urandom",password = None):
+    print(user,password)
